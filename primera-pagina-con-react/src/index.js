036780572004dc32e7bd1953ce8f0b9396abc8db
crcascr <cristianj_20@hotmail.com>
2023-09-21 14:20:48 -0500

    Componentes a medida

--- primera-pagina-con-react/src/index.js
+++ primera-pagina-con-react/src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 //import App from './App';
-import reportWebVitals from './reportWebVitals';
-import logo from './logo192.png';
+import reportWebVitals from "./reportWebVitals";
+import logo from "./logo192.png";
 
-const page = (
+/*const page = (
   <div>
     <img src={logo} alt="Logo" style={{height:'40px'}}/>
       <h1>Fun facts about React</h1>
@@ -17,12 +17,33 @@ const page = (
           <li>Powers thousands of enterprise apps, including mobile apps</li>
       </ul>
   </div>
-)
+)*/
 
 //document.getElementById("root").append(JSON.stringify(page))
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(page);
+function NombreTemporal() {
+  return (
+    <div>
+      <img src={logo} alt="Logo" style={{ height: "40px" }} />
+      <h1>Por que estoy emocionado de aprender React</h1>
+      <ol>
+        <li>Es un nuevo paradigma para mi en cuanto a javaScript</li>
+        <li>Me encanta aprender nuevas cosas</li>
+        <li>
+          Parece tener buena salida laboral lo que es perfecto puesto que estoy
+          desempleado
+        </li>
+        <li>
+          Siento que puede abrirme la puerta para trabajar dentro del mundo del
+          desarrollo que sinceramente, ¡me encanta!
+        </li>
+      </ol>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<NombreTemporal />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
