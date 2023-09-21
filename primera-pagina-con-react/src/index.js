f58dadc43ae342db0b15a0ad0fd1276384f3d48b
crcascr <cristianj_20@hotmail.com>
2023-09-21 13:50:42 -0500

    Proyecto parte 1

--- primera-pagina-con-react/src/index.js
+++ primera-pagina-con-react/src/index.js
@@ -3,17 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 //import App from './App';
 import reportWebVitals from './reportWebVitals';
+import logo from './logo192.png';
 
 const page = (
   <div>
-      <h1>My awesome website in React</h1>
-      <h3>Reasons I love React</h3>
-      <ol>
-          <li>It's composable</li>
-          <li>It's declarative</li>
-          <li>It's a hireable skill</li>
-          <li>It's actively maintained by skilled people</li>
-      </ol>
+    <img src={logo} alt="Logo" style={{height:'40px'}}/>
+      <h1>Fun facts about React</h1>
+      <ul>
+          <li>Was first realeased in 2013</li>
+          <li>Was originally created by Jordan Walke</li>
+          <li>Has well over 100K stars on Github</li>
+          <li>Is maintained by Facebook</li>
+          <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
   </div>
 )
 
