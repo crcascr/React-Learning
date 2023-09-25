e1f339ae18d2ad3eabc3b863f9b2554a940c215b
crcascr <cristianj_20@hotmail.com>
2023-09-25 12:32:03 -0500

    Desestructuracion de props

--- airbnb-experiences-clon/src/components/Contact.js
+++ airbnb-experiences-clon/src/components/Contact.js
@@ -2,18 +2,18 @@ import React from "react";
 import phoneIcon from "../images/contacts/phone-icon.png";
 import mailIcon from "../images/contacts/mail-icon.png";
 
-function Contact(props) {  
+function Contact({img,name,phone,email}) {  
   return (
     <div className="contact-card">
-      <img src={props.img} />
-      <h3>{props.name}</h3>
+      <img src={img} />
+      <h3>{name}</h3>
       <div className="info-group">
         <img src={phoneIcon} />
-        <p>{props.phone}</p>
+        <p>{phone}</p>
       </div>
       <div className="info-group">
         <img src={mailIcon} />
-        <p>{props.email}</p>
+        <p>{email}</p>
       </div>
     </div>
   );
