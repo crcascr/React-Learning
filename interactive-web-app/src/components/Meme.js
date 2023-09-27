085091e23aab98564702d16f0ec021dcc76b9af3
crcascr <cristianj_20@hotmail.com>
2023-09-27 18:20:08 -0500

    Objetos en useState

--- interactive-web-app/src/components/Meme.js
+++ interactive-web-app/src/components/Meme.js
@@ -2,7 +2,6 @@ import React from "react";
 import memesData from "../memesData";
 
 function Meme() {
-  let urlMeme;
   /*function saludo(nombre) {
     const fechaCompleta = new Date();
     const hora = fechaCompleta.getHours();
@@ -21,15 +20,19 @@ function Meme() {
     console.log("Good",momentoDia,nombre);
     
   }*/
+  const [url, setUrl] = React.useState("");
   function recogeClick() {
     const memesLenght = memesData.data.memes.length;
 
-    const numAleatorio = Math.floor(Math.random() * (memesLenght + 1));
+    const numAleatorio = Math.floor(Math.random() * memesLenght);
+    console.log(numAleatorio);
 
-    urlMeme = memesData.data.memes[numAleatorio].url;
-    console.log(urlMeme);
+    const urlMeme = memesData.data.memes[numAleatorio].url;
+    console.log("Url obtenida:", urlMeme);
+    setUrl(urlMeme);
     //saludo("Cristian");
   }
+  /*console.log(url);
 
   const [result, setResult] = React.useState("Yes");
   //console.log(result);
@@ -50,11 +53,18 @@ function Meme() {
   const [contador, setContador] = React.useState(0);
 
   function restarContador() {
-    setContador(contadorAnterior=>contadorAnterior-1);
+    setContador((contadorAnterior) => contadorAnterior - 1);
   }
 
   function sumarContador() {
-    setContador(contadorAnterior=>contadorAnterior+1);
+    setContador((contadorAnterior) => contadorAnterior + 1);
+  }
+
+  //Ternaria en JavaScript
+  const [isGoingOut,setIsGoingOut] = React.useState(true);
+
+  function changeMind(){
+    setIsGoingOut(prevMind=>!prevMind)
   }
 
   return (
@@ -73,20 +83,108 @@ function Meme() {
         <button className="meme--boton" onClick={recogeClick}>
           Obtener un nuevo meme🖼️
         </button>
-        <button className="meme--boton" onClick={agregarElemento}>
-          Agregar elemento
+      </div>
+      <img className="meme--imagen" src={url} />
+      <div className="state" onClick={changeMind}>
+        <h1 className="state--title">Do I feel like going out tonight?</h1>
+        <div className="state--value">
+          <h1>{isGoingOut ? "Yes" : "No"}</h1>
+        </div>
+      </div>
+    </main>
+  );
+ */
+
+  //Matrices
+  /*
+    const [arregloCosas,setArregloCosas]=React.useState(["Cosa 1", "Cosa 2"])
+
+    const elementos = arregloCosas.map((thing) => <p key={thing}>{thing}</p>);
+
+    //function agregarItem(){
+    //    setArregloCosas(prevArregloCosas=>[...prevArregloCosas,`Cosa ${prevArregloCosas.length+1}`])
+    //}
+
+    const[algo,setAlgo]=React.useState(["Algo 1","Algo 2","Algo 3"])
+
+    const cosas=algo.map((cosa)=><p key={cosa}>{cosa}</p>)
+
+    function agregarItem(){
+        setAlgo(prevAlgo=>[...prevAlgo,`Algo ${prevAlgo.length+1}`])
+    }
+
+  return (
+    <main className="meme">
+      <div className="meme--form">
+        <input
+          type="text"
+          className="meme--texto"
+          placeholder="Callese"
+        ></input>
+        <input
+          type="text"
+          className="meme--texto"
+          placeholder="Y tome mi dinero"
+        ></input>
+        <button className="meme--boton" onClick={recogeClick}>
+          Obtener un nuevo meme🖼️
         </button>
-        {elementos}
-        <div className="state--value" onClick={cambiarResultado}>
-          <h1>{result}</h1>
-        </div>
-        <div className="counter">
-          <button className="counter--minus" onClick={restarContador}>–</button>
-          <div className="counter--count">
-            <h1>{contador}</h1>
+      </div>
+      <img className="meme--imagen" src={url} />
+      <button className="meme--boton" onClick={agregarItem}>Agregar Item</button>
+      {cosas}
+    </main>
+  );
+  */
+
+  //Objetos en useState
+  
+  const [contact, setContact] = React.useState({
+    firstName: "John",
+    lastName: "Doe",
+    phone: "+1 (719) 555-1212",
+    email: "itsmyrealname@example.com",
+    isFavorite: false,
+  });
+
+  let estrella=contact.isFavorite ?"star-filled.png":"star-empty.png"
+  
+
+  function toggleFavorite() {
+    console.log("Toggle Favorite");
+  }
+  return (
+    <main className="meme">
+      <div className="meme--form">
+        <input
+          type="text"
+          className="meme--texto"
+          placeholder="Callese"
+        ></input>
+        <input
+          type="text"
+          className="meme--texto"
+          placeholder="Y tome mi dinero"
+        ></input>
+        <button className="meme--boton" onClick={recogeClick}>
+          Obtener un nuevo meme🖼️
+        </button>
+      </div>
+      <img className="meme--imagen" src={url} />
+      <button className="meme--boton">Agregar Item</button>
+      <div className="objetosUseState" style={{ marginTop: "2rem" }}>
+        <article className="card">
+          <div className="card--info">
+            <h2 className="card--favorite" onClick={toggleFavorite}>
+              Marcar favorito ${estrella}
+            </h2>
+            <h2 className="card--name">
+              {contact.firstName} {contact.lastName}
+            </h2>
+            <p className="card--contact">{contact.phone}</p>
+            <p className="card--contact">{contact.email}</p>
           </div>
-          <button className="counter--plus" onClick={sumarContador}>+</button>
-        </div>
+        </article>
       </div>
     </main>
   );
