d73d6302b59d35bdd769f9beb6ec9b316b757099
crcascr <cristianj_20@hotmail.com>
2023-09-28 17:11:11 -0500

    Parte 3.1 - Desafio de las cajas (estado local)

--- interactive-web-app/src/components/Cajas/Caja.js
+++ interactive-web-app/src/components/Cajas/Caja.js
@@ -1,18 +1,23 @@
 import React from "react";
 
-function Caja(props){
-    console.log(props)
-    const estilos = {
-        backgroundColor: props.activa ? "#222222" : "none",
-        color: props.activa ? "#ffffff" : "#222222",
-      };
+function Caja(props) {
+  const [caja, setCaja] = React.useState(props);
+  function cambiarColor() {
+    setCaja((prevCaja) => ({
+      ...prevCaja,
+      activa: !prevCaja.activa,
+    }));
+  }
+  const estilos = {
+    backgroundColor: caja.activa ? "#222222" : "transparent",
+    color: caja.activa ? "#ffffff" : "#222222",
+  };
 
-    return(
-        <div style={estilos} className="caja">
-        {props.numero}        
-      </div>
-    )
-
+  return (
+    <div style={estilos} className="caja" onClick={cambiarColor}>
+      {caja.numero}
+    </div>
+  );
 }
 
-export default Caja
+export default Caja;
