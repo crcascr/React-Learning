f8bb177d01be7d84daf29e564e5869cefc2a1932
crcascr <cristianj_20@hotmail.com>
2023-09-29 07:47:31 -0500

    Parte 4 - Desafio de las cajas

--- interactive-web-app/src/components/Cajas/AppCajas.js
+++ interactive-web-app/src/components/Cajas/AppCajas.js
@@ -7,11 +7,35 @@ function AppCajas(props) {
   const [cajas, setCajas] = React.useState(datosCajas);
 
   function cambiarColor(numero) {
-    console.log(`clic capturado de`,numero)
+    const cajaSeleccionada = cajas.find((caja) => caja.id === numero);
+
+    setCajas((prevCajas) => {
+      const nuevasCajas = [];
+      for (let i = 0; i < prevCajas.length; i++) {
+        const cajaActual = prevCajas[i];
+        if (cajaActual.id === numero) {
+          const cajaActualizada = {
+            ...cajaActual,
+            on: !cajaActual.on,
+          };
+          nuevasCajas.push(cajaActualizada);
+        } else {
+          nuevasCajas.push(cajaActual);
+        }
+      }
+      return nuevasCajas;
+    });
   }
 
   const elementos = cajas.map((caja) => {
-    return <Caja key={caja.id} activa={caja.on} numero={caja.id} invertirColor={cambiarColor} />;
+    return (
+      <Caja
+        key={caja.id}
+        activa={caja.on}
+        numero={caja.id}
+        invertirColor={cambiarColor}
+      />
+    );
   });
   return <main className="mainCajas">{elementos}</main>;
 }
