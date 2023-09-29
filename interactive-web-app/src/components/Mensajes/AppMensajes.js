7b14962d15dc0f0492fcfdac00704de0b8e926e6
crcascr <cristianj_20@hotmail.com>
2023-09-29 09:51:32 -0500

    Práctica de renderizado condicional

--- interactive-web-app/src/components/Mensajes/AppMensajes.js
+++ interactive-web-app/src/components/Mensajes/AppMensajes.js
@@ -1,12 +1,17 @@
 import React from "react";
 
 function AppMensajes() {
-  const [mensajes, setMensajes] = React.useState(["a", "b"]);
+  const [mensajes, setMensajes] = React.useState(["a","b","c"]);
 
   return (
     <div>
-      {mensajes.length > 0 && (
-        <h1>Tienes {mensajes.length} mensajes sin leer!</h1>
+      {mensajes.length > 0 ? (
+        <h1>
+          Tienes {mensajes.length}{" "}
+          {mensajes.length === 1 ? "mensaje" : "mensajes"} sin leer!
+        </h1>
+      ) : (
+        <h1>Estás al día!</h1>
       )}
     </div>
   );
