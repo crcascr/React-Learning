33ee219369e97a5a8d4c3078224330f134152d1b
crcascr <cristianj_20@hotmail.com>
2023-10-06 15:04:41 -0500

    Configuracion estilo principal juego Tenzies

---
+++ practice-projects/tenzies-game/src/AppTenzies.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function AppTenzies(){
+    return(
+        <main>
+            <h1>Aquí irá el juego</h1>            
+        </main>
+    )
+}
+
+export default AppTenzies
