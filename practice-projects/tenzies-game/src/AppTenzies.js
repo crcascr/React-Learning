a72697d9a888a9e1c78c2d2dcea698cf2e80a164
crcascr <cristianj_20@hotmail.com>
2023-10-07 08:07:21 -0500

    Numeros como objetos y key generado con nanoid

--- practice-projects/tenzies-game/src/AppTenzies.js
+++ practice-projects/tenzies-game/src/AppTenzies.js
@@ -1,37 +1,34 @@
 import React from "react";
 import Arreglo from "./Components/Arreglo";
+import { nanoid } from "nanoid";
 
 function AppTenzies() {
+  function todosNumerosDado() {
+    const arregloNumeros = [];
 
-function todosNumerosDado(){
-    const arregloNumeros=[]
-
-    for(let i=0;i<10;i++){
-        const numeroDado=Math.ceil(Math.random()*6)
-        arregloNumeros.push(numeroDado)
+    for (let i = 0; i < 10; i++) {
+      const numeroDado = Math.ceil(Math.random() * 6);
+      arregloNumeros.push({ valor: numeroDado, ocupado: false, id: nanoid() });
     }
-    return arregloNumeros
-    
-}
+    return arregloNumeros;
+  }
 
-const[numeros,setNumeros]=React.useState(todosNumerosDado())
+  const [numeros, setNumeros] = React.useState(todosNumerosDado());
 
-const elementoNumero=numeros.map((numero)=>{
-    return(
-        <Arreglo numero={numero}/>
-    )
-})
+  const elementoNumero = numeros.map((numero) => {
+    return <Arreglo numero={numero.valor} key={numero.id} />;
+  });
 
-function mezclar(){
-    setNumeros(todosNumerosDado())
-}
+  function mezclar() {
+    setNumeros(todosNumerosDado());
+  }
 
   return (
     <main>
-      <div className="numeros">
-        {elementoNumero}
-      </div>
-      <button className="botonMezclar" onClick={mezclar}>Mezclar</button>
+      <div className="numeros">{elementoNumero}</div>
+      <button className="botonMezclar" onClick={mezclar}>
+        Mezclar
+      </button>
     </main>
   );
 }
