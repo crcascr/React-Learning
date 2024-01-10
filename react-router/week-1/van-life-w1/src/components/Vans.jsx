2fac7b9f320f1298337bea8ae6d681cf06424ab5
crcascr <cristianj_20@hotmail.com>
2024-01-09 20:13:19 -0500

    Vans component improvement with data and styles

--- react-router/week-1/van-life-w1/src/components/Vans.jsx
+++ react-router/week-1/van-life-w1/src/components/Vans.jsx
@@ -1,7 +1,40 @@
+import { useState, useEffect } from "react";
+
 function Vans() {
+  const [vansData, setVansData] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/vans")
+      .then((response) => response.json())
+      .then((json) => setVansData(json.vans));
+  }, []);
+
+  const vansObjects = vansData.map((van) => {
+    return (
+      <div key={van.id} className="vans--van">
+        <img
+          className="vans-van-image"
+          alt={`Van ${van.name}`}
+          src={van.imageUrl}
+        />
+        <div className="vans--van-details">
+          <p className="vans--van-name">{van.name}</p>
+          <div className="vans--van-priceContainer">
+            <p className="vans--van-price">${van.price}</p>
+            <p className="vans--van-complement">/day</p>
+          </div>
+        </div>
+        <p className={`vans--van-type ${van.type}`}>
+          {van.type.charAt(0).toUpperCase() + van.type.slice(1)}
+        </p>
+      </div>
+    );
+  });
+
   return (
     <div className="vans">
-      <h1>Here go the vans</h1>
+      <h1 className="vans--title">Explore our van options</h1>
+      <div className="vans--container">{vansObjects}</div>
     </div>
   );
 }
