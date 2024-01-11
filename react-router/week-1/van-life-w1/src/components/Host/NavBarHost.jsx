b2fa3d54122532793eaf7e38e9e98c629b00ab32
crcascr <cristianj_20@hotmail.com>
2024-01-10 19:02:06 -0500

    NavBarHost styling with NavLink

--- react-router/week-1/van-life-w1/src/components/Host/NavBarHost.jsx
+++ react-router/week-1/van-life-w1/src/components/Host/NavBarHost.jsx
@@ -1,12 +1,33 @@
-import { Link} from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function NavBarHost() {
-  
   return (
     <nav className="navbarHost">
-      <Link className="navbarHost--link" to="/host">Dashboard</Link>
-      <Link className="navbarHost--link" to="/host/income">Income</Link>
-      <Link className="navbarHost--link" to="/host/reviews">Reviews</Link>
+      <NavLink
+        className={({ isActive }) =>
+          isActive ? "navbarHost--link hostSelected" : "navbarHost--link"
+        }
+        to="/host"
+        end
+      >
+        Dashboard
+      </NavLink>
+      <NavLink
+        className={({ isActive }) =>
+          isActive ? "navbarHost--link hostSelected" : "navbarHost--link"
+        }
+        to="/host/income"
+      >
+        Income
+      </NavLink>
+      <NavLink
+        className={({ isActive }) =>
+          isActive ? "navbarHost--link hostSelected" : "navbarHost--link"
+        }
+        to="/host/reviews"
+      >
+        Reviews
+      </NavLink>
     </nav>
   );
 }
