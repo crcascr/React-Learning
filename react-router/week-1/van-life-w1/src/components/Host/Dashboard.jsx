c5b6c4f37c606a1d8b1255630df8d6c29afeb458
crcascr <cristianj_20@hotmail.com>
2024-01-11 10:46:53 -0500

    Host vans real data implementation

--- react-router/week-1/van-life-w1/src/components/Host/Dashboard.jsx
+++ react-router/week-1/van-life-w1/src/components/Host/Dashboard.jsx
@@ -1,4 +1,5 @@
 import { Outlet } from "react-router-dom";
+import "./Host.css";
 
 function Dashboard(){
     return(
--- react-router/week-1/van-life-w1/src/components/Host/HostVans.jsx
+++ react-router/week-1/van-life-w1/src/components/Host/HostVans.jsx
@@ -1,5 +1,39 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
 function HostVans() {
-  return <h1>Your listed vans</h1>;
+  const [hostVansData, setHostVansData] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/host/vans")
+      .then((response) => response.json())
+      .then((json) => setHostVansData(json.vans));
+  }, []);
+
+  const hostVansObjects = hostVansData.map((van) => {
+    return (
+      <div key={van.id} className="hostVans--van">
+        <Link to={`/host/vans/${van.id}`} className="hostVans--van-link">
+          <img
+            className="hostVans--van-image"
+            alt={`Van ${van.name}`}
+            src={van.imageUrl}
+          />
+          <div className="hostVans--van-details">
+            <p className="hostVans--van-name">{van.name}</p>
+            <p className="hostVans--van-price">${van.price}/day</p>
+          </div>
+        </Link>
+      </div>
+    );
+  });
+
+  return (
+    <div className="hostVans">
+      <h1 className="hostVans--title">Your listed vans</h1>
+      <>{hostVansObjects}</>
+    </div>
+  );
 }
 
 export default HostVans;
