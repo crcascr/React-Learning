23d126baf343080fc4cfe3754e0baf0c3c4c5ee4
crcascr <cristianj_20@hotmail.com>
2024-01-23 16:03:54 -0500

    Get previous route and redirect to that after successful login

--- react-router/week-5/van-life-w5.1/utils.js
+++ react-router/week-5/van-life-w5.1/utils.js
@@ -1,9 +1,12 @@
 import { redirect } from "react-router-dom";
 
-export async function requireAuth() {
+export async function requireAuth(request) {
+  const pathName = new URL(request.url).pathname;
   const isLoggedIn = localStorage.getItem("loggedin");
 
   if (!isLoggedIn) {
-    throw redirect("/login?message=You must be logged in first");
+    throw redirect(
+      `/login?message=You must be logged in first&redirectTo=${pathName}`
+    );
   }
 }
